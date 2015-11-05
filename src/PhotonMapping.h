1261a0b6af0dc2454ba7c482ddba5814694579c6
JeffOwOSun <sunyushi9546@gmail.com>
2015-11-05 23:29:36 +0800

    Complete implementation for photon mapping generation.

--- src/PhotonMapping.h
+++ src/PhotonMapping.h
@@ -95,23 +95,25 @@ void generatePhotonMap(PhotonMap<T> &point, const Scene* scene, const size_t N, 
 	std::uniform_int_distribution<int> light_random(0, lights.size() - 1);
 	std::uniform_real_distribution<double> uniform_dist(0, 1.0);
 	size_t count = 0;
+
 	while (count < N);
 	{
 		//randomly pick a light source
 		const Light* light = lights[light_random(generator)];
+		//the stack for refracton
+		double cumulative_index = light->getCumulativeIndex();
 		//emit a photon
 		PointLight* point_light = dynamic_cast<PointLight*>(light);
 		if (point_light) { //the light is indeed a point_light
-			//generate a random direction
-			
-
-			//getting position of point_light with a hack
+			//get the photon ray vector
 			ray r = point_light->getPhoton(generator);
+			//the intensity of the photon
 			vec3f intensity = point_light->getColor();
 			isect i;
 			//use a loop to try the intersection progressively
 			while (scene->intersect(r, i);) {
 				//look at the material of the intersection point.
+				//use Russian Roulette to determine the fate of this photon
 				//reference:page16@https://www.siggraph.org/sites/default/files/sample-course-notesa.pdf
 				Material m = i.getMaterial();
 				double maxI = max(intensity);
@@ -123,30 +125,80 @@ void generatePhotonMap(PhotonMap<T> &point, const Scene* scene, const size_t N, 
 				double epsilon = uniform_dist(generator);
 				if (epsilon <= pr / p) {
 					//reflected
+					//modify the intensity
+					intensity = m.kr.prod(intensity) / pr;
+					//continue the tracing with new ray
+					vec3f dir = r.getDirection();
+					dir = dir + 2 * dir.dot(i.N) * i.N;
+					r = ray(r.at(i.t), dir);
 				}
 				else if (epsilon <= (pr + pt) / p) {
-					//transmitted
+					//continue the tracing with new ray
+					//consider the refraction
+					if (i.obj->hasInterior()) {
+						double cos_i = r.getDirection().dot(i.N);
+						double relative_index;
+						if (cos_i > RAY_EPSILON) { //the ray is going out
+							relative_index = i.getMaterial().index;
+							//flip the normal to point to the side of incident. Justified for later calculation of direction
+							i.N = -i.N; 
+						}
+						else { //the ray is going in
+							relative_index = 1 / i.getMaterial().index;
+							cos_i = -cos_i; //make sure it's positive, again for dir calculation
+						}
+						double sin_i = sqrt(1 - cos_i*cos_i);
+						double sin_r = sin_i * relative_index;
+
+						if (sin_r > 1) { //total reflection
+							//continue tracing with reflected ray
+							vec3f dir = r.getDirection();
+							dir = dir + 2 * dir.dot(i.N) * i.N;
+							r = ray(r.at(i.t), dir);
+						}
+						else {
+							//continue tracing with refracted ray
+							double cos_r = sqrt(1 - sin_r * sin_r);
+							//calculate the direction
+							vec3f dir = (relative_index * cos_i - cos_r) * i.N + relative_index * r.getDirection();
+							r = ray(r.at(i.t), dir);
+							//attenune the intensity of refracted ray
+							intensity = m.kt.prod(intensity) / pt;
+							//accumulate the relative_index
+							cumulative_index /= relative_index;
+						}
+					}
+					else { //don't consider thickless shapes
+						r = ray(r.at(i.t), r.getDirection());
+					}
 				}
 				else if (epsilon <= (pr + pt + ps) / p) {
 					//specular reflected
+					intensity = m.ks.prod(intensity) / ps;
+					//continue the tracing with new ray
+					vec3f dir = r.getDirection();
+					dir = dir + 2 * dir.dot(i.N) * i.N;
+					r = ray(r.at(i.t), dir);
 				}
 				else if (epsilon <= (pr + pt + ps + pd) / p) {
 					//diffused
+					intensity = m.kd.prod(intensity) / pd;
+					//remember the intensity
+					vec3f loc = r.at(i.t);
+					point.pts[count].x = loc[0];
+					point.pts[count].y = loc[1];
+					point.pts[count].z = loc[2];
+					point.pts[count].energy = intensity;
 				}
 				else {
 					//absorbed
+					break;
 				}
  			}
-			
 		}
-		else {
+		else {//other types of light source here
 			//ignore them for now!
 		}
-		
-
-		point.pts[i].x = max_range * (rand() % 1000) / T(1000);
-		point.pts[i].y = max_range * (rand() % 1000) / T(1000);
-		point.pts[i].z = max_range * (rand() % 1000) / T(1000);
 	}
 
 	std::cout << "done\n";
